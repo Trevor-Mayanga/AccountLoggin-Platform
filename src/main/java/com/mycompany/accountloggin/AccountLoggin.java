34e0b11fa01075ecdee9505f7a96a8599ee083b6
Trecor-Mayanga <ST10537862@myemeris.edu.za>
2026-04-12 16:32:25 +0200

    Test units for password and username

--- src/main/java/com/mycompany/accountloggin/AccountLoggin.java
+++ src/main/java/com/mycompany/accountloggin/AccountLoggin.java
@@ -5,6 +5,7 @@
 package com.mycompany.accountloggin;
 
 import java.util.regex.Pattern;
+import java.util.HashMap;
 
 /**
  *
@@ -12,22 +13,86 @@ import java.util.regex.Pattern;
  */
 public class AccountLoggin {
     
-    // Check usernamm: must contain _ and maximum of 8 charectors 
+    // where the details will stored [password & username]
+private static HashMap<String, String[]> userDatabase = new HashMap<>();
+
     
-public static boolean checkusername(String username ){
-    return username.contains("_")&& username.length()<=8;
+  // Check usernamm: must contain _ and maximum of 8 charectors 
+    
+public static boolean checkUsername(String username ){
+    return username.contains("_")&& username.length()<=5;
             }
 //check password: must contain: a cap latter + a num + a special charector
 
-public static boolean cheackpassword(char[] password){
+public static boolean checkPasswordComplexity(char[] password){
     if (password == null) return false;
     String passW=new String(password);
     // regex: at least has one digit + one cap letter + one special charector
-    String regex = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\":{}|<>]).{8,}$";
+    String regex = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?\":{}|<>]).{8}$";
         return Pattern.matches(regex, passW);
+}
+        
+//check cellphone number: must be south african
+ 
+public static boolean checkCellPhoneNumber(String phone){
+    String phone1=new String(phone);
+    String regex = ("^\\+27[0-9]{9}$");
+    return Pattern.matches(regex,phone1);
+    
+    };
+
+// Registering the usre 
+
+ public static String registerUser(String username, String surname, String password, String phone) {
+        if (!checkUsername(username)) {
+            return "Username incorrectly formatted, please ensure that your username contains an underscore and is no more than 5 characters long.";
+        }
+        if (!checkPasswordComplexity(password.toCharArray())) {
+            return "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character.";
+        }
+        if (!checkCellPhoneNumber(phone)) {
+            return "The cellphone number is incorrectly formatted.";
+        }
+
+        // Store user: Username is the key, [password, surname] is the value
+        userDatabase.put(username, new String[]{password, surname});
+        return "The two items have been registered successfully.";
+   }
+ //Method to login user
+ 
+ public static boolean loginUser(String username,String password){
+     if (userDatabase.containsKey(username)){
+            return userDatabase.get(username)[0].equals(password);}
+     return false;
+ }
+ 
+ 
+ // Method to return loggin status
+ 
+ public static String returnLoginStatus(String username, String password){
+     if  (loginUser(username,password)) {
+          String surname = userDatabase.get(username)[1];
+         return "WELCOME " + username + " " + surname + " IT IS NICE TO SEE YOU AGAIN";
+     }else {
+         return "USERNAME OR PASSWORD INCORRECT PLEASE TRY AGAIN";
+         }
+     
+     
+   
+  
+ 
+ }
+ 
+
+
+
+
+
+        
+        
    
      
-    }
+    
 
 
 
@@ -41,4 +106,4 @@ public static boolean cheackpassword(char[] password){
     public static void main(String[] args) {
         
     }
-}
+};
---
+++ src/test/java/com/mycompany/accountloggin/AccountLogginTest.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.accountloggin;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class AccountLogginTest {
+    
+  @Test
+  // test to see if kyl_1 works(which should) 
+  
+  public void testUsernameWithUnderscorIsValid(){
+      boolean result = AccountLoggin.checkUsername("kyl_1");
+      assertTrue(result);
+  }
+  
+ @Test
+ // test to see if kyle works (it should not work)
+ 
+ public void testUsernameWithoutUnderscorIsValid(){
+     boolean result = AccountLoggin.checkUsername("kyle!!!!!!!");
+     assertFalse(result);
+ }
+  
+@Test
+// test to see if Ch&&sec@ke99!meets the requirments(it should work)
+
+public void testPasswordThatMeetsTheRequirments(){                
+    boolean result = AccountLoggin.checkPasswordComplexity("Ch&&sec@ke99!".toCharArray());
+    assertTrue(result);
+}
+
+@Test
+// test to see if "password" Meets the requirmets (which will not)
+
+public void testPasswordThatDoesNotMeetTheRequirmets(){
+    boolean result = AccountLoggin.checkPasswordComplexity("password".toCharArray());
+    assertFalse(result);
+
+}
+
+
+  
+}
